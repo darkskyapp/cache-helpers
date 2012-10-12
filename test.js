a1b5c0faac1e0f32f5c90af4201b8a1df94a9bfc
Jason LaPorte <ironwallaby@gmail.com>
2012-10-12 17:05:45 -0400

    Added a test for the k-v cache.

--- test.js
+++ test.js
@@ -2,65 +2,127 @@ var assert = require("chai").assert,
     cache  = require("./")
 
 describe("cache", function() {
-  var n
+  describe("time-based", function() {
+    var n
 
-  function inc(callback) {
-    return callback(null, n++)
-  }
+    function inc(callback) {
+      return callback(null, n++)
+    }
 
-  beforeEach(function() {
-    n = 0
-  })
+    beforeEach(function() {
+      n = 0
+    })
 
-  describe("once", function() {
-    it("should only call the backing cache function once", function() {
-      var cinc = cache.once(inc)
+    describe("once", function() {
+      it("should only call the backing cache function once", function() {
+        var cinc = cache.once(inc)
 
-      cinc(function(err, n) {
-        assert.equal(n, 0)
+        cinc(function(err, n) {
+          assert.equal(n, 0)
+        })
+
+        cinc(function(err, n) {
+          assert.equal(n, 0)
+        })
+
+        cinc(function(err, n) {
+          assert.equal(n, 0)
+        })
       })
 
-      cinc(function(err, n) {
-        assert.equal(n, 0)
+      /* FIXME: Ensure that calling the function a bajillion times causes each
+       * callback to get called. */
+    })
+
+    describe("timeBasedWithGrace", function() {
+      it("should only update the cache as appropriate", function() {
+        var cinc = cache.timeBasedWithGrace(inc, 100, 1000)
+
+        /* First datapoint. */
+        cinc(0, function(err, n) {
+          assert.deepEqual(n, 0)
+        })
+
+        /* Read cached. */
+        cinc(50, function(err, n) {
+          assert.deepEqual(n, 0)
+        })
+
+        /* Soft timeout: return cached but update in the background. */
+        cinc(200, function(err, n) {
+          assert.deepEqual(n, 0)
+        })
+
+        /* Read cached. */
+        cinc(250, function(err, n) {
+          assert.deepEqual(n, 1)
+        })
+
+        /* Hard timeout: update n before returning. */
+        cinc(2500, function(err, n) {
+          assert.deepEqual(n, 2)
+        })
       })
 
-      cinc(function(err, n) {
-        assert.equal(n, 0)
-      })
+      /* FIXME: Ensure that calling the function a bajillion times causes each
+       * callback to get called. */
     })
   })
 
-  describe("timeBasedWithGrace", function() {
-    it("should only update the cache as appropriate", function() {
-      var cinc = cache.timeBasedWithGrace(inc, 100, 1000)
+  describe("key-value", function() {
+    var n
 
-      /* First datapoint. */
-      cinc(0, function(err, n) {
-        assert.deepEqual(n, 0)
-      })
+    function inc(key, callback) {
+      return callback(null, key ^ n++)
+    }
 
-      /* Read cached. */
-      cinc(50, function(err, n) {
-        assert.deepEqual(n, 0)
-      })
-
-      /* Soft timeout: return cached but update in the background. */
-      cinc(200, function(err, n) {
-        assert.deepEqual(n, 0)
-      })
-
-      /* Read cached. */
-      cinc(250, function(err, n) {
-        assert.deepEqual(n, 1)
-      })
-
-      /* Hard timeout: update n before returning. */
-      cinc(2500, function(err, n) {
-        assert.deepEqual(n, 2)
-      })
+    beforeEach(function() {
+      n = 0
     })
 
-    /* FIXME: Ensure that calling the function a bajillion times causes each
-     * callback to get called. */
+    describe("sizeBasedKeyValue", function() {
+      it("should cache for each key, until you fill the space", function() {
+        var f = cache.sizeBasedKeyValue(inc, 2)
+
+        f(20, function(err, value) {
+          assert.equal(value, 20)
+        })
+
+        f(20, function(err, value) {
+          assert.equal(value, 20)
+        })
+
+        f(40, function(err, value) {
+          assert.equal(value, 41)
+        })
+
+        f(20, function(err, value) {
+          assert.equal(value, 20)
+        })
+
+        f(40, function(err, value) {
+          assert.equal(value, 41)
+        })
+
+        f(30, function(err, value) {
+          assert.equal(value, 28)
+        })
+
+        f(40, function(err, value) {
+          assert.equal(value, 41)
+        })
+
+        f(20, function(err, value) {
+          assert.equal(value, 23)
+        })
+
+        f(40, function(err, value) {
+          assert.equal(value, 41)
+        })
+      })
+
+      /* FIXME: Ensure that calling the function a bajillion times causes each
+       * callback to get called. */
+    })
   })
 })
